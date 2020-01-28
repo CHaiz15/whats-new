7f77f9b4e57bbbd99c4d370fdd7e70d602d52654
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-28 14:51:25 -0700

    Add key to news articles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -4,7 +4,7 @@ import NewsArticle from '../NewsArticle/NewsArticle.js'
 
 const NewsContainer = (props) => {
   let articles = props.localNews.map(news => {
-    return <NewsArticle headline={news.headline} img={news.img} url={news.url} description={news.description} />
+    return <NewsArticle key={news.headline} headline={news.headline} img={news.img} url={news.url} description={news.description} />
   })
   return <div className='NewsContainer'>
     {articles}
