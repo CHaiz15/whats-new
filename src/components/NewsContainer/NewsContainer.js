f643756cc2cb814f9e1fc912d7d2e83977d22e5e
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-28 12:33:40 -0700

    Add news data manipulation and display

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './NewsContainer.css'
 import NewsArticle from '../NewsArticle/NewsArticle.js'
 
-const NewsContainer = () => {
-  
-  return <div> </div>;
+const NewsContainer = (props) => {
+  let articles = props.localNews.map(news => {
+    return <NewsArticle headline={news.headline} img={news.img} url={news.url} description={news.description} />
+  })
+  return <div className='NewsContainer'>
+    {articles}
+  </div>;
 }
 
 export default NewsContainer;
