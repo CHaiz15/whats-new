9507c91334d6eb22f3c111aa12f373f37bc4e31c
Caleb Haizlett <caleb.haizlett@gmail.com>
2020-01-30 15:17:57 -0700

    Add onClick run function test

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -10,4 +10,14 @@ describe('Menu', () => {
 
     expect(wrapper).toMatchSnapshot();
   })
+  it('should call the searchArticles prop with the search value when the button is clicked', () => {
+    const mockUpdateSource = jest.fn();
+    const wrapper = shallow(
+      <Menu
+        updateSource={mockUpdateSource}
+      />
+    );
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockUpdateSource).toHaveBeenCalled();
+  })
 })
